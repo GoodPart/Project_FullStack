67532fbd3641d5eec0de0929557d23285ba936a4
cttd1421 <cttd1421@gepplus.co.kr>
2019-11-22 11:24:03 +0900

    image

--- src/App.js
+++ src/App.js
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import MuiThemProvider from '@material-ui/core/styles/MuiThemeProvider'
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme'
+//component
+import Navbar from './components/Navbar';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import home from './pages/home';
+import login from './pages/login'
+import signup from './pages/signup'
+
+
+const theme = createMuiTheme({
+  palette : {
+    primary: {
+      light : '#33c9dc',
+      main : '#00bcd4',
+      dark : '#008394',
+      contrastText : '#fff'
+    },
+    secondary : {
+      light : '#ff6333',
+      main : '#ff3d00',
+      dark : '#b22a00',
+      contrastText : '#fff'
+    }
+  },
+  typography: {
+    useNextVariants : true
+  }
+}) 
+
+
+
+class App extends Component {
+  render() {
+    return (
+      <MuiThemProvider theme={theme}>
+        <div className="App">
+        <Router>
+          <div className="container">
+          <Navbar />
+          <Switch>
+            <Route exact path="/" component={home} />
+            <Route path="/login" component={login} />
+            <Route path="/signup" component={signup} />
+          </Switch>
+          </div>
+        </Router >
+        
+      </div>
+      </MuiThemProvider>
+    )
+  }
 }
 
 export default App;
